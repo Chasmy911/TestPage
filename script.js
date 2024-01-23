58ade2009293db6619c406b359fd703369159713
chasmy911 <chasmy@inbox.ru>
2024-01-23 09:38:14 +0300

    add styles

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
- 
+const burgerLine = document.querySelector('.burger-menu');
+const burgerList = document.querySelector('.menu__list');
+const burgerMenu = document.querySelector('.header__navigation');
+
+const burgerItem = document.querySelectorAll('.header__navigation-item');
+
+burgerLine.addEventListener('click', () => {
+  burgerMenu.classList.toggle('hamburger__menu__active');
+
+  burgerLine.classList.toggle('active');
+})
+
+
+for (let i = 0; i < burgerItem.length; i++) {
+  burgerItem[i].addEventListener('click', () => {
+    burgerMenu.classList.remove('hamburger__menu__active');
+
+    burgerLine.classList.remove('active');
+  })
+}
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
